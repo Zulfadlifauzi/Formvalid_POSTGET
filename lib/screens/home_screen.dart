02154be315ecfca818cc036f0603ffe57054e808
Zulfadlifauzi <fauzi_zulfadli@yahoo.com>
2021-11-10 10:32:01 +0800

    add boxes

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:moduleapp/models/list_module.dart';
-import 'package:moduleapp/screens/form_valid.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -23,104 +21,33 @@ class _ProfileScreenState extends State<HomeScreen> {
         centerTitle: true,
         backgroundColor: Colors.black,
       ),
-      body: SingleChildScrollView(
-        child: Container(
-          width: double.infinity,
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 20,
-              ),
-              Container(
-                padding: const EdgeInsets.only(left: 20, right: 20),
-                child: Row(
-                  children: const [
-                    Text('Widgets'),
-                  ],
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                Container(
+                  color: Colors.red,
+                  width: 180,
+                  height: 180,
+                  child: const Text('POST'),
                 ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Row(
-                children: [
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: MediaQuery.of(context).size.height,
-                    child: ListView(
-                      scrollDirection: Axis.vertical,
-                      children: [
-                        Container(
-                          width: 20,
-                          height: 40,
-                          padding: EdgeInsets.only(left: 20, right: 20),
-                          child: Row(
-                            children: const [
-                              Text('1.'),
-                              SizedBox(
-                                width: 5,
-                              ),
-                              Text('Scaffold')
-                            ],
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        Container(
-                          height: 40,
-                          padding: EdgeInsets.only(left: 20, right: 20),
-                          child: Row(
-                            children: const [
-                              Text('2.'),
-                              SizedBox(
-                                width: 5,
-                              ),
-                              Text('Appbar')
-                            ],
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        Container(
-                          height: 40,
-                          color: Colors.blue,
-                          padding: EdgeInsets.only(left: 20, right: 20),
-                          child: Row(
-                            children: const [
-                              Text('3.'),
-                              SizedBox(
-                                width: 5,
-                              ),
-                              Text('Floating Action Button')
-                            ],
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        Container(
-                          height: 40,
-                          color: Colors.blue,
-                          padding: EdgeInsets.only(left: 20, right: 20),
-                          child: Row(
-                            children: const [
-                              Text('4.'),
-                              SizedBox(
-                                width: 5,
-                              ),
-                              Text('Drawer')
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ],
-          ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Container(
+                  color: Colors.blue,
+                  width: 180,
+                  height: 180,
+                  child: const Text('POST'),
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
