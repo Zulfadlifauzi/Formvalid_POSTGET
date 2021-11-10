b47f8838f78edec651ac0955879ed1a5f2be11ec
Zulfadlifauzi <fauzi_zulfadli@yahoo.com>
2021-11-10 14:35:37 +0800

    ad validation in form

--- lib/screens/post_screen.dart
+++ lib/screens/post_screen.dart
@@ -13,8 +13,7 @@ class _SignupScreenState extends State<PostDataScreen> {
   TextEditingController emailController = TextEditingController();
   TextEditingController nameController = TextEditingController();
 
-  final _formKey = GlobalKey<FormState>();
-  final scaffoldKey = GlobalKey<FormState>();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -24,103 +23,117 @@ class _SignupScreenState extends State<PostDataScreen> {
           'POST Data to API',
           style: GoogleFonts.staatliches(letterSpacing: 5),
         ),
-        key: scaffoldKey,
         backgroundColor: Colors.black,
         centerTitle: true,
       ),
       backgroundColor: const Color(0xFFffffff),
       body: SingleChildScrollView(
-        child: Form(
-          key: _formKey,
-          child: Container(
-            padding: const EdgeInsets.only(left: 40, right: 40),
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              Center(
-                child: Column(
-                  children: const [
-                    SizedBox(
-                      height: 50,
-                    ),
-                    Text(
-                      'Post Data to API',
-                      style: TextStyle(
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 20),
-              TextFormField(
-                autovalidateMode: AutovalidateMode.always,
-                controller: nameController,
-                decoration: InputDecoration(
-                  focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5)),
-                  enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5)),
-                  labelText: 'Enter your name',
-                ),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Name required';
-                  } else {
-                    return null;
-                  }
-                },
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                controller: emailController,
-                decoration: InputDecoration(
-                  focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5)),
-                  enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5)),
-                  labelText: 'Enter your email',
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                obscureText: true,
-                controller: passController,
-                decoration: InputDecoration(
-                  focusedBorder: OutlineInputBorder(
-                      // borderSide: BorderSide(color: Colors.blue),
-                      borderRadius: BorderRadius.circular(5)),
-                  enabledBorder: OutlineInputBorder(
-                    // borderSide: BorderSide(color: Colors.blue),
-                    borderRadius: BorderRadius.circular(5),
+        child: Center(
+          child: Form(
+            key: _formKey,
+            child: Container(
+              padding: const EdgeInsets.only(left: 40, right: 40),
+              child: Column(children: [
+                Center(
+                  child: Column(
+                    children: const [
+                      SizedBox(
+                        height: 50,
+                      ),
+                      Text(
+                        'Post Data to API',
+                        style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black),
+                      ),
+                    ],
                   ),
-                  labelText: 'Enter your Pasword',
                 ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {}
+                const SizedBox(height: 20),
+                TextFormField(
+                  autovalidateMode: AutovalidateMode.always,
+                  controller: nameController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5)),
+                    labelText: 'Enter your name',
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty ||
+                        !RegExp(r'^[a-z A-Z]+$').hasMatch(value)) {
+                      return 'Please enter your name';
+                    } else if (value.isEmpty) {
+                      return 'Required';
+                    } else {
+                      return null;
+                    }
                   },
-                  child: Container(
-                    width: double.infinity,
-                    child: const Text(
-                      'Submit',
-                      textAlign: TextAlign.center,
-                    ),
-                  ))
-            ]),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  autovalidateMode: AutovalidateMode.always,
+                  controller: emailController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5)),
+                    labelText: 'Enter your email',
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter your email';
+                    } else if (value.isNotEmpty ||
+                        !RegExp('^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]')
+                            .hasMatch(value)) {
+                      return 'Invalid email';
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  autovalidateMode: AutovalidateMode.always,
+                  obscureText: true,
+                  controller: passController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5)),
+                    labelText: 'Enter your Pasword',
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter your password';
+                    } else if (value.length <= 5 && value.isNotEmpty) {
+                      return 'At least 6 characters';
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {}
+                    },
+                    child: Container(
+                      width: double.infinity,
+                      child: const Text(
+                        'Submit',
+                        textAlign: TextAlign.center,
+                      ),
+                    ))
+              ]),
+            ),
           ),
         ),
       ),
     );
   }
-
-//ignore: must_be_immutable
 }
